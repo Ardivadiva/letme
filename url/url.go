61a490882e65877ca80a3255ad6cf9acb992062c
Rolly Maulana Awangga <rolly@awang.ga>
2023-07-02 16:29:34 +0700

    dd

--- url/url.go
+++ url/url.go
@@ -12,4 +12,8 @@ func Web(page *fiber.App) {
 	page.Get("/ws/whatsauth/qr", websocket.New(controller.WsWhatsAuthQR)) //websocket whatsaut
 	page.Get("/", controller.GetHome)
 	page.Get("/undangan", controller.GetDataUndanganRapat)
+	page.Get("/tam", controller.GetDataListTamu)
+	page.Get("/peserta", controller.GetDataPesertaRapat)
+	page.Get("/rapat", controller.GetDataRapatMulai)
+	page.Get("/waktu", controller.GetDataWaktuRapat)
 }
